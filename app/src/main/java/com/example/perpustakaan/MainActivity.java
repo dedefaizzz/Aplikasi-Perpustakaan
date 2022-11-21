3b9e4eb0a8764d3ed26bc4f03f24dd7450d34ea6
DEDE CAHYADI <111202113415@mhs.dinus.ac.id>
2022-11-21 22:39:14 +0700

    Edit mainActivity, penambahan library, method setupListView, method onItemClick, dan method onResume

--- app/src/main/java/com/example/perpustakaan/MainActivity.java
+++ app/src/main/java/com/example/perpustakaan/MainActivity.java
@@ -1,48 +1,56 @@
 package com.example.perpustakaan;
 
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.example.perpustakaan.databinding.ActivityMainBinding;
-
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.appcompat.widget.Toolbar;
+
+import com.example.perpustakaan.adapter.CustomCursorAdapter;
+import com.example.perpustakaan.adapter.DBHelper;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
+    ListView ls;
+    DBHelper dbHelper;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setSupportActionBar(toolbar);
 
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivity(new Intent(MainActivity.this,AddActivity.class));
             }
         });
+
+        dbHelper = new DBHelper(this);
+        ls = (ListView) findViewById(R.id.list_pinjam);
+        ls.setOnClickListener((View.OnClickListener) this);
+
+        setupListView();
+    }
+
+    private void setupListView() {
+        Cursor cursor = dbHelper.AllData();
+        CustomCursorAdapter customCursorAdapter = new CustomCursorAdapter(this, cursor, 1);
+        ls.setAdapter(customCursorAdapter);
     }
 
     @Override
@@ -67,10 +75,19 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l){
+        TextView getID = (TextView) view.findViewById(R.id.listID);
+        final long id = Long.parseLong(getID.getText().toString());
+        Cursor cur = dbHelper.oneData(id);
+        cur.moveToFirst();
+
+        Intent idpinjam = new Intent(MainActivity.this, AddActivity.class);
+        idpinjam.putExtra(DBHelper.row_id,id);
+        startActivity(idpinjam);
+    }
+
+    public void onResume(){
+        super.onResume();
+        setupListView();
     }
 }
