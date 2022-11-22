9247d66edc0a28699d3f3877cec6e35d65b035f6
DEDE CAHYADI <111202113415@mhs.dinus.ac.id>
2022-11-22 22:19:36 +0700

    Penambahan di AddActivity.java lanjutan mulai dari onSupportNavigateUp() smp insertAndUpdate()

--- app/src/main/java/com/example/perpustakaan/AddActivity.java
+++ app/src/main/java/com/example/perpustakaan/AddActivity.java
@@ -1,5 +1,6 @@
 package com.example.perpustakaan;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +11,8 @@ import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -151,6 +154,111 @@ public class AddActivity extends AppCompatActivity {
                 TxtglKembali.setEnabled(false);
                 TxStatus.setEnabled(false);
             }
+        }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.add_menu, menu);
+        String idpinjam = TxID.getText().toString().trim();
+        String status = TxStatus.getText().toString().trim();
+
+        MenuItem itemDelete = menu.findItem(R.id.action_delete);
+        MenuItem itemClear = menu.findItem(R.id.action_clear);
+        MenuItem itemSave = menu.findItem(R.id.action_save);
+
+        if (idpinjam.equals("")){
+            itemDelete.setVisible(false);
+            itemClear.setVisible(true);
+        } else {
+            itemDelete.setVisible(true);
+            itemClear.setVisible(false);
+        }
+
+        if (status.equals("Dikembalikan")){
+            itemDelete.setVisible(false);
+            itemClear.setVisible(false);
+            itemSave.setVisible(false);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_save:
+                insertAndUpdate();
+        }
+        switch (item.getItemId()){
+            case R.id.action_clear:
+                TxNama.setText("");
+                TxJudul.setText("");
+                TxtglKembali.setText("");
+        }
+        switch (item.getItemId()){
+            case R.id.action_delete:
+                final AlertDialog.Builder builder = new AlertDialog.Builder(AddActivity.this);
+                builder.setMessage("Data ini akan dihapus!");
+                builder.setCancelable(true);
+                builder.setPositiveButton("Hapus", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                           dbHelper.deleteData(id);
+                           Toast.makeText(AddActivity.this, "Terhapus", Toast.LENGTH_SHORT).show();
+                           finish();
+                    }
+                });
+
+                builder.setNegativeButton("Batal", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                             dialogInterface.cancel();
+                    }
+                });
+
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void insertAndUpdate() {
+        String idpinjam = TxID.getText().toString().trim();
+        String nama = TxNama.getText().toString().trim();
+        String judul = TxJudul.getText().toString().trim();
+        String tglPinjam = TxtglPinjam.getText().toString().trim();
+        String tglKembali = TxtglKembali.getText().toString().trim();
+        String status = "Dipinjam";
+
+        ContentValues values = new ContentValues();
+        values.put(DBHelper.row_nama, nama);
+        values.put(DBHelper.row_judul, judul);
+        values.put(DBHelper.row_kembali, tglKembali);
+        values.put(DBHelper.row_status, status);
+        
+        if (nama.equals("") || judul.equals("") ||  tglKembali.equals("")){
+            Toast.makeText(AddActivity.this, "Silahkan mnegisi data dengan lengkap!", Toast.LENGTH_SHORT).show();
+        } else {
+            if (idpinjam.equals("")){
+                values.put(DBHelper.row_pinjam, tglPinjam);
+                dbHelper.insertData(values);
+            } else {
+                dbHelper.updateData(values, id);
+            }
+            Toast.makeText(AddActivity.this, "data Tersimpan", Toast.LENGTH_SHORT).show();
+            finish();
         }
     }
 }
